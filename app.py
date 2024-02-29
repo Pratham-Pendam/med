d72606cc6e09b3decf71a010f0f849bb06b56a0c
unknown <airbenderkitz@gmail.com>
2024-03-01 03:54:52 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+import faiss
+
+from langchain.vectorstores import FAISS
+
+app = Flask(__name__)
+
+load_dotenv()
+
+def load_pdf(data):
+    loader = DirectoryLoader(data,
+                    glob="*.pdf",
+                    loader_cls=PyPDFLoader)
+    
+    documents = loader.load()
+
+    return documents
+
+extracted_data = load_pdf("data/")
+
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+
+    return text_chunks
+
+text_chunks = text_split(extracted_data)
+print("length of my chunk:", len(text_chunks))
+embeddings = download_hugging_face_embeddings()
+
+
+db = FAISS.from_documents( text_chunks,embeddings)
+
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+chain_type_kwargs={"prompt": PROMPT}
+
+
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8})
+
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=db.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True, 
+    chain_type_kwargs=chain_type_kwargs)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
+
+
